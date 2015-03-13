40db3a987db677208b99661fbcc61a655013db58
zenit710 <zenit710@gmail.com>
2015-03-14 00:59:53 +0100

    teraz możemy zacząć działać

--- AllegroManager/src/com/bestteam/allegromanager/MainActivity.java
+++ AllegroManager/src/com/bestteam/allegromanager/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
-		Log.d("km","test filip Kamil i od nowa magiczne rêce");
+		Log.d("km","test filip Kamil i od nowa magiczne rêce, goœæ jest mistrzem œwiata");
 	}
 
 	@Override
